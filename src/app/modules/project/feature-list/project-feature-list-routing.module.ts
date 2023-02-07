fb073129f3b0654bd164cc6dfd570a92953c2382
Ali Hasani <alihasani.get@gmail.com>
2023-02-07 22:24:35 +0330

    chore: Modify some content chore: Modify some routes feat: Add some properties to project model

--- src/app/modules/project/feature-list/project-feature-list-routing.module.ts
+++ src/app/modules/project/feature-list/project-feature-list-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthorizedGuard } from '../../core';
 import { ProjectListComponent } from './project-list/project-list.component';
 
 const routes: Routes = [
   {
-    canActivate: [AuthorizedGuard],
     path: '',
     component: ProjectListComponent,
   },
