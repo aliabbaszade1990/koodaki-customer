550a7588ac123cf42ecf286ff159cd5909ad0630
Ali Hasani <alihasani.get@gmail.com>
2023-02-09 20:10:01 +0330

    chore: Modify some content

--- src/app/modules/project/data-access/dtos/get-file.dto.ts
+++ src/app/modules/project/data-access/dtos/get-file.dto.ts
@@ -2,7 +2,8 @@ import { CurrentItem } from '../interfaces/current-item-interface';
 
 export interface GetFileDto extends CurrentItem {
   id: string;
-  src: string;
+  name: string;
   selected: boolean;
   comment: string;
+  url: string;
 }
