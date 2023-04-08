4c1f3ca59d554ffc5ad08c12e4ba879b86c039de
Ali Hasani <alihasani.get@gmail.com>
2023-04-08 18:16:14 +0330

    Modify some configurations Delete a file

--- src/environments/environment.dev.ts
+++
@@ -1,7 +0,0 @@
-import { Environment } from './interfaces/environment.interface';
-
-export const environment: Environment = {
-  name: 'DEV',
-  production: false,
-  api: 'https://www.api.koodakistudio.com',
-};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { Environment } from './interfaces/environment.interface';
 export const environment: Environment = {
   name: 'PROD',
   production: true,
-  api: 'https://www.api.koodakistudio.com',
+  api: 'https://www.api.koodakistudio.ir',
 };
